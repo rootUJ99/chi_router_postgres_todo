efbf416909fd5eeedb34ed7736f4baa86624f5be
ujwal arak <ujwal99arak@gmail.com>
2023-08-26 14:57:23 +0530

    added all crud requests

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/rootuj99/chi_router_server
 go 1.20
 
 require (
+	github.com/ajg/form v1.5.1 // indirect
 	github.com/go-chi/chi/v5 v5.0.10 // indirect
 	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/go-chi/render v1.0.3 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -13,12 +15,45 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type Todo struct {
+	Name string `json:"name"`
+	Id   int    `json:"id"`
+}
+
+type querries struct {
+	create string
+	update string
+	get    string
+	delete string
+}
+type Row struct {
+	Name string `json:"name"`
+	Id   string `json:"id"`
+}
+
+func returnError(w http.ResponseWriter, message string) {
+	type ErrorMessage struct {
+		ErrMessage string `json:"errMsg"`
+	}
+	w.WriteHeader(400)
+	json.NewEncoder(w).Encode(ErrorMessage{ErrMessage: message})
+}
+
+func crudQuerry(key string, value string, key2 int) querries {
+	q := querries{
+		create: fmt.Sprintf("insert into todos (%v) values ('%v') returning %v;", key, value, key),
+		update: fmt.Sprintf("update todos set %v = '%v' where id=%v returning %v;", key, value, key2, key),
+		get:    fmt.Sprintf("select name, id from todos;"),
+		delete: fmt.Sprintf("delete from todos where id=%v returning %v;", key2, key),
+	}
+	return q
+}
+
 func main() {
 	godotenv.Load(".env")
-
 	fmt.Println("hello from go project")
+
 	database_url := os.Getenv("DATABASE_URL")
-	fmt.Println(database_url, "url here")
 	conn, err := pgx.Connect(context.Background(), database_url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "unable to connect to database %v\n", err)
@@ -27,21 +62,21 @@ func main() {
 
 	defer conn.Close(context.Background())
 
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select current_database()").Scan(&greeting)
+	var dbname string
+	err = conn.QueryRow(context.Background(), "select current_database()").Scan(&dbname)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(greeting)
+	fmt.Printf("connected to the database %v\n", dbname)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(
 		cors.Options{
 			AllowedOrigins:   []string{"http://*", "https://*"},
-			AllowedMethods:   []string{"GET", "POST", "PUT", "OPTIONS"},
+			AllowedMethods:   []string{"GET", "POST", "PUT", "OPTIONS", "DELETE"},
 			AllowedHeaders:   []string{"Content-Type", "Accept", "Autorization"},
 			ExposedHeaders:   []string{"link"},
 			AllowCredentials: false,
@@ -53,24 +88,108 @@ func main() {
 		w.Write([]byte("helloz"))
 	})
 
-	r.Post("/healthy-path", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("this is a healthy path"))
+	r.Get("/get-todos", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		query := crudQuerry("", "", 0).get
+		fmt.Println(query)
+		row, err := conn.Query(context.Background(), query)
+		if err != nil {
+			returnError(w, "query error")
+		}
+		defer row.Close()
+		var rowSlice []Row
+		for row.Next() {
+			var r Row
+			if err := row.Scan(&r.Id, &r.Name); err != nil {
+				returnError(w, "query error")
+
+			}
+			rowSlice = append(rowSlice, r)
+
+		}
+		fmt.Println(rowSlice)
+
+		err = json.NewEncoder(w).Encode(&rowSlice)
+		if err != nil {
+			returnError(w, "server error")
+
+		}
+
 	})
 
-	r.Get("/create-todo/{todo}", func(w http.ResponseWriter, r *http.Request) {
-		todo := chi.URLParam(r, "todo")
-		query := fmt.Sprintf("insert into todos (name) values ('%v') returning name;", todo)
-		fmt.Println(query)
-		var finalString string
-		err := conn.QueryRow(context.Background(), query).Scan(&finalString)
-		if err != nil {
-			msg := fmt.Sprintf("QueryRow failed: %v\n", err)
-			w.WriteHeader(400)
-			w.Write([]byte(msg))
-			return
+	r.Post("/create-todo", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		type todoBody struct {
+			Name string `json:"name"`
 		}
-		fmt.Println(finalString)
-		w.Write([]byte(finalString))
+		var todobody todoBody
+		if err := json.NewDecoder(r.Body).Decode(&todobody); err != nil {
+			returnError(w, "parsing error")
+		}
+		query := crudQuerry("name", todobody.Name, 0).create
+		fmt.Println(query)
+		var name string
+		err := conn.QueryRow(context.Background(), query).Scan(&name)
+		if err != nil {
+			returnError(w, "query error")
+		}
+		type resMesg struct {
+			Message string `json:"message"`
+		}
+
+		err = json.NewEncoder(w).Encode(resMesg{Message: fmt.Sprintf("new %v has been created", name)})
+		if err != nil {
+			returnError(w, "server error")
+		}
+
+	})
+
+	r.Put("/update-todo", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var todo Todo
+		if err := json.NewDecoder(r.Body).Decode(&todo); err != nil {
+			returnError(w, "parsing error")
+		}
+		query := crudQuerry("name", todo.Name, todo.Id).update
+		var name string
+		if err := conn.QueryRow(context.Background(), query).Scan(&name); err != nil {
+			returnError(w, "query error")
+		}
+
+		type resMesg struct {
+			Message string `json:"message"`
+		}
+
+		err = json.NewEncoder(w).Encode(resMesg{Message: fmt.Sprintf("%v has been updated", name)})
+		if err != nil {
+			returnError(w, "server error")
+		}
+
+	})
+
+	r.Delete("/delete-todo/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		id := chi.URLParam(r, "id")
+		intId, err := strconv.Atoi(id)
+		if err != nil {
+			returnError(w, "wrong id format")
+		}
+		query := crudQuerry("name", "", intId).delete
+		var name string
+		if err := conn.QueryRow(context.Background(), query).Scan(&name); err != nil {
+			returnError(w, "query error")
+		}
+
+		type resMesg struct {
+			Message string `json:"message"`
+		}
+
+		err = json.NewEncoder(w).Encode(resMesg{Message: fmt.Sprintf("%v has been deleted", name)})
+		if err != nil {
+			returnError(w, "server error")
+		}
 
 	})
 
